e5c567d57da89e18bf6f3581630e214a0a8c4caa
ArtuCN <147415284+ArtuCN@users.noreply.github.com>
2024-01-15 19:38:12 +0100

    Update fractol.h

--- fractol/fractol.h
+++ fractol/fractol.h
@@ -45,12 +45,33 @@
 
 
 //define key
-#define M 109
+#define A 97
+#define B 98
+#define C 99
+#define D 100
+#define E 101
+#define F 102
+#define G 103
+#define H 104
+#define I 105
+#define J 106
 #define K 107
 #define L 108
+#define M 109
 #define N 110
-#define J 106
-#define F 102
+#define O 111
+#define P 112
+#define Q 113
+#define R 114
+#define S 115
+#define T 116
+#define U 117
+#define V 118
+#define W 119
+#define X 120
+#define Y 121
+#define Z 122
+
 #define LEFT 65361
 #define RIGHT 65363
 #define UP 65362
