924ebb94f9e2761866901acc56d3daa903094197
ArtuCN <147415284+ArtuCN@users.noreply.github.com>
2024-01-20 13:09:12 +0100

    Update events.c

--- fractol/events.c
+++ fractol/events.c
@@ -9,7 +9,6 @@
 /*   Updated: 2024/01/13 13:34:32 by aconti           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "fractol.h"
 
 int close_window(t_fractal *fractal)
@@ -20,18 +19,8 @@ int close_window(t_fractal *fractal)
 						fractal->mlx_window);
 	mlx_destroy_display(fractal-> mlx_connection);
 	free(fractal-> mlx_connection);
-    ft_putstr("\nFinestra chiusa! :)");
+    ft_putstr("\nFinestra chiusa! :)\n");
 	exit(EXIT_SUCCESS);
-}
-
-int	mouse_handler(int button, int x, int y, t_fractal *fractal)
-{
-	if (button == Button4)
-		fractal->zoom *= 0.95;
-	else if (button == Button5)
-		fractal->zoom *= 1.05;
-	fractal_render(fractal);
-	return (0);
 }
 
 int	key_handler(int keysym, t_fractal *fractal)
@@ -47,7 +36,7 @@ int	key_handler(int keysym, t_fractal *fractal)
 	else if (keysym == DOWN)
 		fractal->shift_y -= (0.5 * fractal->zoom);
 	else if (!ft_strncmp(fractal->name, "julia", 5) && keysym == L)
-		fractal->julia_x -= (0.25 * fractal->zoom);
+		fractal->julia_x -= (0.5 * fractal->zoom);
 	else if (!ft_strncmp(fractal->name, "julia", 5) && keysym == K)
 		fractal->julia_x += (0.25 * fractal->zoom);
 	else if (!ft_strncmp(fractal->name, "julia", 5) && keysym == N)
@@ -55,32 +44,67 @@ int	key_handler(int keysym, t_fractal *fractal)
 	else if (!ft_strncmp(fractal->name, "julia", 5) && keysym == M)
 		fractal->julia_y += (0.25 * fractal->zoom);
 	else if (keysym == J)
-	{
 		fractal->name = "julia";
-		fractal->zoom = 0.0;
-		fractal->shift_x = 0.0;
-		fractal->shift_y = 0.0;
-	}
 	else if (keysym == F)
-	{
 		fractal->name = "mandelbrot";
-		fractal->zoom = 0.0;
-		fractal->shift_x = 0.0;
-		fractal->shift_y = 0.0;
-	}
-	else if (keysym == R)
+	else if (keysym == Q)
 	{
-		fractal->zoom = 0.0;
+		fractal->zoom = 1.0;
 		fractal->shift_x = 0.0;
-		fractal->shift_y = 0.0;
+		fractal->shift_y = 0.0;		
+		fractal->image.green = 1;
+		fractal->image.blue = 1;
+		fractal->image.red = 1;
 	}
+	else if(keysym == P)
+		fractal->mod += 100;
+	else if(keysym == O)
+		fractal->mod -= 100;
+	else if (keysym == X)
+		fractal->name = "bonus";
+	if (keysym == B || keysym == R || keysym == G)
+		my_color_change(keysym, fractal);
+	mlx_clear_window(fractal->mlx_connection, fractal->mlx_window);
 	fractal_render(fractal);
 	return (0);
 }
+
+int mouse_track(int x, int y, t_fractal *fractal)
+{
+	fractal->mouse_imag = y;
+	fractal->mouse_real = x;
+
+	return (0);
+}
+
+int mouse_handler(int button, int x, int y, t_fractal *fractal)
+{
+	double mouse_real;
+    double mouse_imag;
+	double new_mouse_real;
+	double new_mouse_imag;
+	
+	mouse_real = map(y, pianox, pianoX, HEIGHT) * fractal->zoom + fractal->shift_x;
+	mouse_imag = map(y, pianoY, pianoy, HEIGHT) * fractal->zoom + fractal->shift_y;
+    if (button == Button4)
+        fractal->zoom *= 0.95;
+    else if (button == Button5)
+        fractal->zoom *= 1.05;
+    new_mouse_real = map(y, pianox, pianoX, HEIGHT) * fractal->zoom + fractal->shift_x;
+    new_mouse_imag = map(y, pianoY, pianoy, HEIGHT) * fractal->zoom + fractal->shift_y;
+    fractal->shift_x += mouse_real - new_mouse_real;
+    fractal->shift_y += mouse_imag - new_mouse_imag;
+    fractal_render(fractal);
+	return (0);
+}
+
+
+
 
 void    events(t_fractal *fractal)
 {
     mlx_key_hook(fractal->mlx_window, key_handler, fractal);
     mlx_hook(fractal->mlx_window, 17, 1L << 2, close_window, fractal);
-    mlx_mouse_hook(fractal->mlx_window, mouse_handler, fractal);
+	mlx_mouse_hook(fractal->mlx_window, mouse_handler, fractal);
+	mlx_hook(fractal->mlx_window, MotionNotify, PointerMotionMask, mouse_track, fractal);
 }
