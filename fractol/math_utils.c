9205b205cbe81a0cf55bd31084f1f62e7ad73c12
ArtuCN <147415284+ArtuCN@users.noreply.github.com>
2024-01-20 13:10:39 +0100

    Update math_utils.c

--- fractol/math_utils.c
+++ fractol/math_utils.c
@@ -9,13 +9,12 @@
 /*   Updated: 2024/01/13 12:21:39 by aconti           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "fractol.h"
 //map utilizzata per scalare i pixel
 
 double map(double unscaled_num, double new_min, double new_max, double old_max)
 {
-	return (new_max - new_min) * (unscaled_num) / (old_max ) + new_min;
+	return (new_max - new_min) * (unscaled_num) / (old_max) + new_min;
 }
 
 t_complex calculate_iteration(t_complex t, t_complex c)
@@ -24,17 +23,14 @@ t_complex calculate_iteration(t_complex t, t_complex c)
 
     result.real = t.real * t.real - t.immaginary * t.immaginary + c.real;
     result.immaginary = 2 * t.real * t.immaginary + c.immaginary;
-
-    return (result);
+	return (result);
 }
 
 
 
 double module(t_complex c)
 {
-    double realPart = c.real;
-    double imagPart = c.immaginary;
-    return (realPart * realPart + imagPart * imagPart);
+    return (c.real * c.real + c.immaginary * c.immaginary);
 }
 
 int isJulia(t_complex n, t_fractal *fractal)
@@ -45,7 +41,7 @@ int isJulia(t_complex n, t_fractal *fractal)
 	constant.immaginary = fractal->julia_y;
 	constant.real = fractal->julia_x;
 	iteration = 0;
-	while (iteration < MAXITERATION)
+	while (iteration < MAXITERATION + fractal->mod)
 	{
 		n = calculate_iteration(n, constant);
 		if (module(n) > 4.0)
@@ -55,7 +51,7 @@ int isJulia(t_complex n, t_fractal *fractal)
 	return (iteration);
 }
 
-int isMandelbrot(t_complex n)
+int isMandelbrot(t_complex n, t_fractal *fractal)
 {
 	t_complex t;
 	int iteration;
@@ -63,10 +59,10 @@ int isMandelbrot(t_complex n)
 	t.immaginary = 0;
 	t.real = 0;
 	iteration = 0;
-	while(iteration < MAXITERATION)
+	while(iteration < (MAXITERATION + fractal->mod))
 	{
 		t = calculate_iteration(t, n);
-		if (module(t) > 32.0)
+		if (module(t) > 8.0)
 			return (iteration);
 		iteration ++;
 	}
