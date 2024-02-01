8cf5743c9b016c55407ebc3f283f095bedc22afa
ArtuCN <147415284+ArtuCN@users.noreply.github.com>
2024-02-01 16:17:06 +0100

    Update check.c

--- push_swap/check.c
+++ push_swap/check.c
@@ -6,55 +6,141 @@
 /*   By: aconti <aconti@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 15:13:57 by aconti            #+#    #+#             */
-/*   Updated: 2024/01/27 18:41:10 by aconti           ###   ########.fr       */
+/*   Updated: 2024/02/01 11:24:04 by aconti           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int count_num(char **nptr)
+{
+    int n = 0;
+    int i = 1;
+    int j;
+    while (nptr[i]) {
+        j = 0;
+        while (nptr[i][j])
+        {
+            if (nptr[i][j] >= '0' && nptr[i][j] <= '9')
+            {
+                while (nptr[i][j] >= '0' && nptr[i][j] <= '9')
+                    j++;
+                n++;
+            }
+            else
+                j++;
+        }
+        i++;
+    }
+    return n;
+}
+
 int is_sign(char av)
 {
-	if (av == '+')
-		return (1);
-    else if ( av == '-')
-        return(-1);
-	else 
-		return (0);
+    if (av == '+')
+        return (1);
+    else if(av == '-')
+        return (-1);
+    else
+        return (0);
 }
 
-int	ft_check_a(t_stack **a)
+int all_num(char **nptr)
 {
-	t_stack    *tmp;
+    int i = 1;
+    int j;
+    while (nptr[i])
+    {
+        j = 0;
+        while (nptr[i][j])
+        {
+            if((nptr[i][j] >= '0' && nptr[i][j] <= '9'|| nptr[i][j] == ' ' || is_sign(nptr[i][j]) != 0))
+                j++;
+            else
+                return 0;
+        }
+        i++;
+    }
+    return 1;
+}
 
-	while (*a)
+int ft_atoi(char **nptr, long int *nums)
+{
+    int i = 1;
+    long int num = 0;
+    int n = 0;
+    while (nptr[i])
+    {
+        int sign = 1;
+        int j = 0;
+        while (nptr[i][j])
+        {
+            if (is_sign(nptr[i][j]) == 1 || is_sign(nptr[i][j]) == -1)
+            {
+                sign = is_sign(nptr[i][j]);
+                j++;
+            }
+            while (nptr[i][j] >= '0' && nptr[i][j] <= '9')
+            {
+                num = num * 10 + (nptr[i][j] - '0');
+                if (nptr[i][j + 1] == ' ' || nptr[i][j + 1] == '\0')
+                {
+                    nums[n] = sign * num;
+                    num = 0;
+                    n++;
+                    if(nptr[i][j + 1] == '\0')
+                        break;
+                }
+                j++;
+            }
+            j++;
+        }
+        i++;
+    }
+    return n;
+}
+
+int ft_check(long int *nums, int i)
+{
+    int j;
+    i--;
+    while (i >= 0)
+    {
+        j = 0;
+        if(nums[i] > 2147483647 || nums[i] < -2147483648)
+            return 0;
+        while (j < i)
+        {
+            if((int)nums[i] == (int)nums[j])
+                return 0;
+            j++;
+        }
+        i--;
+    }
+    return (1);
+}
+
+void print_stack(t_stack *stack)
+{
+    while (stack != NULL) {
+        printf("Value: %ld, Index: %d\n", stack->val, stack->index);
+        stack = stack->next;
+    }
+    printf("\n");
+}
+
+void array_to_stack(long int *arr, int size, t_stack **stack)
+{
+	t_stack *new_node;
+	int i = size - 1;
+	while (i >= 0)
 	{
-		tmp = (*a)->next;
-		while (tmp)
-		{
-			if ((*a)->val == tmp->val)
-			{
-				printf("Errore nell'inserimento!\n");
-				return (0);
-			}
-			tmp = tmp->next;
-		}
-		*a = (*a)->next;
+		new_node = malloc(sizeof(t_stack));
+		new_node->val = arr[i];
+		new_node->index = i;
+		new_node->next = *stack;
+		*stack = new_node;
+		i--;
 	}
-	return (1);
 }
-
-t_stack *add_stack(t_stack **a, int num, int j)
-{
-    if (num > 2147483647 || num < -2147483648)
-        return NULL;
-
-    t_stack *new_node = malloc(sizeof(t_stack));
-    if (!new_node)
-        return NULL;
-    new_node->val = num;
-    new_node->index = j;
-    new_node->next = *a; // Link the new node to the existing list
-    return new_node;
-}
-
 
