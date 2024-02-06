c2f14d707cbb2bbb300e12012b4ec298d703e498
ArtuCN <147415284+ArtuCN@users.noreply.github.com>
2024-02-06 15:42:12 +0100

    Update cont.c

--- push_swap/cont.c
+++ push_swap/cont.c
@@ -13,24 +13,74 @@
 #include "push_swap.h"
 
 
+void little_check(t_stack **a, t_stack **b)
+{
+	int i;
+	
+	i = 0;
+	if (!a || !(*a) || !b || !(*b))
+    	return ;
+	if (!(*a)->next || !(*b)->next)
+		return ;	
+	while ((*a)->val < (*b)->val && *b && (*b)->next)
+	{
+		rotate(a);
+		write(1, "ra\n", 3);
+		i++;
+	}
+	pa(a, b);
+	while (i-- > 0)
+	{
+		reverse_rotate(a);	
+		write (1, "rra\n", 4);
+	}
+}
+
 void cont(t_stack **a, t_stack **b)
 {
-	
 	int i = counter(*b);
 	t_stack *temp;
 	int mediat;
-
 	while (i >= 3)
 	{
 	    temp = *b;
+		if (!temp->next || !temp || !b || !(*b) || !(*b)->next || !(*a)->next)
+			break ;
     	mediat = media(*b);
-		if (temp->val < temp->next->val)
-    		check_ss(a, b);
-    	if (mediat <= temp->val)
-    	    pa(a, b);
+		while (temp->val < temp->next->val || (*a)->val > (*a)->next->val)
+			check_ss(a, b);
+    	if (mediat < temp->val)
+			little_check(a, b);
 	    else
         	do_rotate(a, b, 2);
 	    i = counter(*b); 
 	}
-	
+	check_ss(a, b);
+	pa(a, b);
+	pa(a, b);
+	//final(a, b);
+}
+
+
+int mid_media(t_stack *a)
+{
+	int med;
+	int res;
+	t_stack *temp;
+	int i;
+
+	i = 0;
+	temp = a;
+	res = 0;
+	med = media(a);
+	while(temp != NULL)
+	{
+		if (temp->val <= med)
+		{
+			res += temp->val;
+			i++;
+		}
+		temp = temp->next;
+	}
+	return (res / i);
 }
