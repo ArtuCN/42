595e14df4f4b95290dda35b0233f85de8bfec117
ArtuCN <147415284+ArtuCN@users.noreply.github.com>
2024-02-09 17:04:24 +0100

    Update sort.c

--- push_swap/sort.c
+++ push_swap/sort.c
@@ -13,6 +13,7 @@
 #include "push_swap.h"
 
 
+
 double	media(t_stack *a)
 {
 	int i;
@@ -46,6 +47,48 @@ int	counter(t_stack *a)
 	return (n);
 }
 
+
+
+int check_push(t_stack **a, int i)
+{
+	t_stack *temp;
+	
+	temp = (t_stack *)malloc(sizeof(t_stack));
+	if (!temp)
+		return (0);
+	temp = *a;
+	while (temp)
+	{
+		if (temp->push == i)
+			return (1);
+		temp = temp->next;
+	}
+	return (0);
+}
+
+
+void pre_order(t_stack **a, t_stack **b)
+{
+	int	i;
+	t_stack	*temp;
+	
+	temp = (t_stack *)malloc(sizeof(t_stack));
+	if (!temp)
+		return ;
+	temp = *a;
+	i = 1;
+	while (i < 6)
+	{
+		if (temp->push == i)
+			pb(b, a);
+		else
+			do_rotate(a, b, 1);
+		temp = *a;
+		if (!check_push(a, i))
+			i++;
+	}
+}
+
 void	sort(t_stack **a, t_stack **b, int n)
 {
 	int	mediat;
@@ -54,6 +97,8 @@ void	sort(t_stack **a, t_stack **b, int n)
 
 	temp = *a;
 	mediat = mid_media(*a);
+	if ((*a)->push != 0)
+		pre_order(a, b);
 	while (counter(temp) > 2)
 	{
 		if (temp->val < mediat)
@@ -63,6 +108,10 @@ void	sort(t_stack **a, t_stack **b, int n)
 		temp = *a;
 		mediat = media(*a);		
 	}
-	temp = *a;
+	if ((*a)->val > (*a)->next->val)
+	{
+		sa(a);
+		write(1, "sa\n", 3);
+	}
 	cont(a, b);
 }
